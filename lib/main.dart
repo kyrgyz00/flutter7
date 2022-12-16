67bb3963ae2734bf75b7036d04fb123a2ecf459c
kyrgyz00 <bekbolottashpolotov@gmail.com>
2022-12-16 18:25:28 +0600

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,90 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Practice7(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Practice7 extends StatefulWidget {
+  const Practice7({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Practice7> createState() => _Practice7State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _Practice7State extends State<Practice7> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Color(0xffD9A8FF),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: height*0.269,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Text("Вход",
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
+                
+            SizedBox(height: height * 0.044),
+            SizedBox(
+              width: width * 0.904,
+              height: height * 0.0615,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "E-mail",
+                  hintStyle: TextStyle(color: Color(0xff4F4F4F),fontSize: 14,fontWeight: FontWeight.w400),
+                    border:
+                        OutlineInputBorder(borderSide: BorderSide(width: 5))),
+              ),
             ),
+            SizedBox(
+              height: height*0.0246,
+            ),
+            SizedBox(
+              width: width * 0.904,
+              height: height * 0.0615,
+              child: TextField(
+                obscuringCharacter: "*",
+                decoration: InputDecoration(
+                  
+                  hintText: "Пароль",
+                  
+                  hintStyle: TextStyle(color: Color(0xff333333),fontSize: 14,fontWeight: FontWeight.w400),
+                  suffix: Icon(Icons.visibility_off,color: Color(0xff333333)),
+                  
+                    border:
+                        OutlineInputBorder(borderSide: BorderSide(width: 5))
+                        
+                        ),
+                        
+              ),
+            ),
+            SizedBox(height: height*0.0812,),
+            Container(
+              width: width*0.352,
+              height: height * 0.0615,
+              decoration: BoxDecoration(
+              color: Color(0xff743F8C),
+                borderRadius: BorderRadius.circular(50)
+              ),
+              alignment: Alignment.center,
+              child: Text("Войти",style: TextStyle(color: Color(0xffFFFFFF), fontSize: 14,fontWeight: FontWeight.w500),),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
